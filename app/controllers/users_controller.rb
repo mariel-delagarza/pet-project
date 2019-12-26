a057b951e49e713554cd4bec6b5bc09bc0841f3c
mariel-delagarza <mariel.code@gmail.com>
2019-12-26 12:37:43 -0600

    Comment in post /login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,15 @@ class UsersController < ApplicationController
     erb :"/users/login"
   end
 
+  # post "/login" do 
+  #   @user = User.find_by(username: params[:username])
+  #   if @user && @user.authenticate(params[:password])
+  #     session[:user_id] = @user.id
+  #     redirect to "/pets"
+  #   else 
+  #     redirect to "/login"
+  #   end 
+  # end 
+
+  
 end 
