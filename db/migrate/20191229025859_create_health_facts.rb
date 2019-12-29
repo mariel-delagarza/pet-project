3e2987cef8720510524a717a87eca53a5bf4f25c
mariel-delagarza <mariel.code@gmail.com>
2019-12-28 21:03:16 -0600

    Add vet name and vet phone

--- db/migrate/20191229025859_create_health_facts.rb
+++ db/migrate/20191229025859_create_health_facts.rb
@@ -1,7 +1,8 @@
 class CreateHealthFacts < ActiveRecord::Migration
   def change
     create_table :health_facts do |t|
-
+      t.string :vet_name 
+      t.string :vet_phone 
       t.timestamps null: false
     end
   end
