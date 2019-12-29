b852245ff2352adf8a7747ae72a4c5798a817066
mariel-delagarza <mariel.code@gmail.com>
2019-12-28 21:09:50 -0600

    Added name, species, breed, y/n microchip, microchip number, microchip registry

--- db/migrate/20191227164634_create_pets.rb
+++ db/migrate/20191227164634_create_pets.rb
@@ -1,7 +1,12 @@
 class CreatePets < ActiveRecord::Migration
   def change
     create_table :pets do |t|
-
+      t.string :name 
+      t.string :species 
+      t.string :breed
+      t.boolean :has_microchip
+      t.string :microchip_number 
+      t.string :microchip_registry 
       t.timestamps null: false
     end
   end
