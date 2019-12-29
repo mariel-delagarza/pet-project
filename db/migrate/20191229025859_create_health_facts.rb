f4b1a282d347069c09d8775caf0231c5db302701
mariel-delagarza <mariel.code@gmail.com>
2019-12-28 21:12:43 -0600

    Add pet_id

--- db/migrate/20191229025859_create_health_facts.rb
+++ db/migrate/20191229025859_create_health_facts.rb
@@ -3,6 +3,7 @@ class CreateHealthFacts < ActiveRecord::Migration
     create_table :health_facts do |t|
       t.string :vet_name 
       t.string :vet_phone 
+      t.integer :pet_id
       t.timestamps null: false
     end
   end
--- db/migrate/20191229025939_create_care_facts.rb
+++ db/migrate/20191229025939_create_care_facts.rb
@@ -4,6 +4,7 @@ class CreateCareFacts < ActiveRecord::Migration
       t.string :food_brand 
       t.string :food_serving 
       t.string :feedings_per_day 
+      t.integer :pet_id 
       t.timestamps null: false
     end
   end
