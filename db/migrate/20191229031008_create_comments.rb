5dc1352f663b983a9d3a53bb4bcd656194e255a4
mariel-delagarza <mariel.code@gmail.com>
2019-12-28 21:12:04 -0600

    Add pet id

--- db/migrate/20191229031008_create_comments.rb
+++ db/migrate/20191229031008_create_comments.rb
@@ -2,6 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :comment 
+      t.integer :pet_id 
       t.timestamps null: false
     end
   end
